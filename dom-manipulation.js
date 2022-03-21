15bc8a27818b4326f1e956ae4ea7845882a8053b
fillahalfatih <m.fiilah.a.f@gmail.com>
2022-03-22 05:26:15 +0700

    display input from usr using .keyup and .append

--- dom-manipulation.js
+++ dom-manipulation.js
@@ -21,8 +21,8 @@ $(document).ready(function() {
     $('ul').append('<li><b>Append</b> list item</li>');
     $('ul').prepend('<li><b>Prepend</b> list item</li>');
     // $('li.list-item-2').appendTo('li.list-item-3');
-    $('ul').before('<h4>Before the <code>ul</code></h4>');
-    $('ul').after('<h4>After the <code>ul</code></h4>');
+    // $('ul').before('<h4>Before the <code>ul</code></h4>');
+    // $('ul').after('<h4>After the <code>ul</code></h4>');
 
     // .empty = Hide an inner elements
     // $('ul').empty();
@@ -38,4 +38,28 @@ $(document).ready(function() {
         $('#container').toggleClass('switch-theme');
         // $('body').toggleClass('switch-theme');
     });
+
+    // target _blank
+    $('a').attr('target', '_blank');
+    // remove attr
+    $('a').removeAttr('target');
+
+    // wrap to an <h1></h1>
+    // $('p').wrap("<h1></h1>");
+    $('p').wrapAll("<h1></h1>");
+
+    // to-do list
+    $('#newItem').keyup(function (e) { 
+        let code = e.which;
+        // key 13 keycode is for ENTER key
+        if (code == 13) { 
+            e.preventDefault();
+            $('ul').append('<li>'+e.target.value+'</li>');
+        }
+    });
+
+    // let myArr = ['Brad', 'Kelley', 'Nate', 'Jose'];
+    // $.each(myArr, function (i, val) {
+    //     $('#users').append('<li>'+val+'</li>');
+    // });
 })
